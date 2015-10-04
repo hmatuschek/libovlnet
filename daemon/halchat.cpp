b184e212480aae06a149b341156244dcffa08005
Hannes Matuschek <hmatuschek@gmail.com>
2015-10-04 22:38:24 +0200

    ...

--- daemon/halchat.cpp
+++ daemon/halchat.cpp
@@ -8,6 +8,7 @@ HalChat::HalChat(DHT &dht, QHalModel &model, QObject *parent)
 
 void
 HalChat::handleDatagram(const uint8_t *data, size_t len) {
+  if ((0 == data) && (0 == len)) { return; }
   QString msg = QString::fromUtf8((const char *)data, len);
   msg = _model.reply(msg);
   QByteArray buffer = msg.toUtf8();
