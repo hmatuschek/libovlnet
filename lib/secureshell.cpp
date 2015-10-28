ee6d4b47dc5841790726320725059483b597b6a0
Hannes Matuschek <hmatuschek@gmail.com>
2015-10-28 10:34:16 +0100

    Added some comments.

--- lib/secureshell.cpp
+++ lib/secureshell.cpp
@@ -3,7 +3,7 @@
 SecureShell::SecureShell(DHT &dht, const QString &command, QObject *parent)
   : SecureStream(dht, parent), _command(command), _process()
 {
-  conenct(&_process, SIGNAL(started()), this, SLOT(_shellStarted()));
+  connect(&_process, SIGNAL(started()), this, SLOT(_shellStarted()));
   connect(&_process, SIGNAL(readyReadStandardOutput()), this, SLOT(_shellReadyRead()));
   connect(&_process, SIGNAL(readyReadStandardError()), this, SLOT(_shellReadyRead()));
   connect(&_process, SIGNAL(bytesWritten(qint64)), this, SLOT(_shellBytesWritten(qint64)));
@@ -20,8 +20,61 @@ SecureShell::open(OpenMode mode) {
   return true;
 }
 
+void
+SecureShell::close() {
+  SecureStream::close();
+  _process.terminate();
+}
 
+void
+SecureShell::_shellStarted() {
+  // pass...
+}
 
+void
+SecureShell::_shellReadyRead() {
+  char buffer[256];
+  while (_process.bytesAvailable()) {
+    qint64 len = 256;
+    len = _process.read(buffer, len);
+    write(buffer, len);
+  }
+}
+
+void
+SecureShell::_shellBytesWritten(qint64 bytes) {
+  char buffer[256];
+  while (bytesAvailable()) {
+    qint64 len = 256;
+    len = read(buffer, len);
+    _process.write(buffer, len);
+  }
+}
+
+void
+SecureShell::_remoteReadyRead() {
+  char buffer[256];
+  while (bytesAvailable()) {
+    qint64 len = 256;
+    len = read(buffer, len);
+    _process.write(buffer, len);
+  }
+}
+
+void
+SecureShell::_remoteBytesWritten(qint64 bytes) {
+  char buffer[256];
+  while (_process.bytesAvailable()) {
+    qint64 len = 256;
+    len = _process.read(buffer, len);
+    write(buffer, len);
+  }
+}
+
+void
+SecureShell::_remoteClosed() {
+  _process.terminate();
+}
 
 
 
--- lib/secureshell.h
+++ lib/secureshell.h
@@ -4,27 +4,44 @@
 #include "stream.h"
 #include <QProcess>
 
-
+/** Implements a trivial secure shell service.
+ * This class simply forwards any data to and from a @c SecureStream connection to a process.
+ * @ingroup services */
 class SecureShell : public SecureStream
 {
   Q_OBJECT
 
 public:
+  /** Constructor.
+   * @param dht Specifies the DHT instance.
+   * @param command Specifies the command to execute.
+   * @param parent Specifies the optional QObject parent. */
   SecureShell(DHT &dht, const QString &command="login", QObject *parent=0);
 
+  /** Startes the process. */
   bool open(OpenMode mode);
+  /** Closes the connection and terminates the process. */
+  void close();
 
 protected slots:
+  /** Gets called once the process started. */
   void _shellStarted();
+  /** Gets called if some data get available from the process. */
   void _shellReadyRead();
+  /** Gets called if some data has been written to the process. */
   void _shellBytesWritten(qint64 bytes);
 
+  /** Gets called if some data has been received from the remote. */
   void _remoteReadyRead();
+  /** Gets called if some data has been send to the remote. */
   void _remoteBytesWritten(qint64 bytes);
+  /** Gets called if the connection to the remote has been closed. */
   void _remoteClosed();
 
 protected:
+  /** The command to execute. */
   QString  _command;
+  /** The process. */
   QProcess _process;
 };
 
